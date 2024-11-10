e9eaec82f2d835ad34754a3fccb7176970fabcb7
Mohammad Fozouni <71451039+fozouni@users.noreply.github.com>
2024-11-10 15:53:05 +0330

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from absenteeism_module import *
 ################################################################
 
 def Input_Output():
-    data = st.file_uploader("Please Upload Your File Here 😂😂😂", type={"csv", "txt"})
+    data = st.file_uploader("لطفن فایل خود را اینجا آپلود نمائید", type={"csv", "txt"})
     if data is not None:
         df = pd.read_csv(data)
         st.write(df)
